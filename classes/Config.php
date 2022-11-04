735a630310fdddf58223c47f634c6e23dd1533fd
Zach Lieberman <zacharylieberman1@gmail.com>
2022-11-04 10:30:17 -0400

    Update Config.php

--- classes/Config.php
+++ classes/Config.php
@@ -4,8 +4,8 @@ class Config {
     
     public static $db = [
         "host" => "localhost:3306",
-        "user" => "zl5nrs",
-        "pass" => "password1",
+        "user" => "USERNAME",
+        "pass" => "PASSWORD",
         "database" => "databases_project" // change this to whatever u named it on localhost
     ];
 }
