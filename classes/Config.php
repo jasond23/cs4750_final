2b5d5b7cf6dc860e1b5042e4c23037b2428a9259
JasonDeng22 <tmdeng00@gmail.com>
2022-11-13 05:34:23 -0500

    gitignore

--- classes/Config.php
+++ classes/Config.php
@@ -3,9 +3,9 @@
 class Config {
     
     public static $db = [
-        "host" => "localhost:3306",
-        "user" => "USERNAME",
-        "pass" => "PASSWORD",
+        "host" => "localhost",
+        "user" => "root",
+        "pass" => "",
         "database" => "databases_project" // change this to whatever u named it on localhost
     ];
 }
